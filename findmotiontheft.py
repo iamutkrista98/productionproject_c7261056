bb738ccd42d28690e1017e2d8eede9bc4c1a6a1c
iamutkrista98 <iamutkrista98@gmail.com>
2023-06-04 14:10:53 +0545

    dummy commit testing out

--- findmotiontheft.py
+++ findmotiontheft.py
@@ -1,42 +1,53 @@
+# importing of necessary libraries and functions
 import cv2
 import time
 import numpy as np
 from datetime import datetime
 from theftdetection import theftdetection
 
+# finding motion function for theft structural similarity scenario
+
+
 def find_motion():
+    # setting of initial boolean status for motions
     motion_detected = False
     is_start_done = False
+    # capture assigned to first indexed camera interfaced to computer
     cap = cv2.VideoCapture(0)
 
     check = []
+    # wait for two seconds and evaluate the surrounding and capture a frame
     print("Waiting for 2 seconds...")
     time.sleep(2)
     _, frame1 = cap.read()
-
+    # converting to grayscale
     frm1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-
+    # capture till condition met continouously
     while True:
         _, frame2 = cap.read()  # Properly retrieve frame2
         frm2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
         diff = cv2.absdiff(frm1, frm2)
         _, thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
-        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(
+            thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = [c for c in contours if cv2.contourArea(c) > 25]
-
+        # when identified countours or edges greater than 5
         if len(contours) > 5:
-            cv2.putText(thresh, "Motion Suspected", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+            cv2.putText(thresh, "Motion Suspected", (50, 50),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
             motion_detected = True
             is_start_done = False
-
+        # when motion detected identified as true and contours at least less than 3
         elif motion_detected and len(contours) < 3:
             if not is_start_done:
                 start = time.time()
                 is_start_done = True
                 end = time.time()
-
+            # assign end time as current time
             end = time.time()
+            # printing of time difference
             print(end - start)
+            # if the time difference between the end and start of mocap is greater than 4
             if (end - start) > 4:
                 cap.release()
                 cv2.destroyAllWindows()
@@ -48,12 +59,15 @@ def find_motion():
                     return
                 else:
                     print("Found Motion")
+
                     return
+        # when no motion has been suspected and no flags set
         else:
-            cv2.putText(thresh, "No Motion Suspected", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+            cv2.putText(thresh, "No Motion Suspected", (50, 50),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
 
         cv2.imshow("Find Motion Mode", thresh)
-
+        # recapture the frame
         _, frm1 = cap.read()
         frm1 = cv2.cvtColor(frm1, cv2.COLOR_BGR2GRAY)
 
--- theftdetection.py
+++ theftdetection.py
@@ -1,3 +1,4 @@
+# importing of necessary libraries, packages and functions
 import cv2
 import time
 from skimage.metrics import structural_similarity
@@ -12,22 +13,26 @@ def theftdetection(frame1, frame2):
     monitoring_directory = 'stolen'
     if not os.path.exists(monitoring_directory):
         os.mkdir(monitoring_directory)
+
+    # conversion into grayscale for both frames
     grayscale1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     grayscale2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-
+    # applying blur as part of image processing on the two frames
     grayscale1 = cv2.blur(grayscale1, (2, 2))
     grayscale2 = cv2.blur(grayscale2, (2, 2))
-
+    # evaluate structural similarity score between the two grayscaled frames
     (score, diff) = structural_similarity(grayscale2, grayscale1, full=True)
-
+    # printing the similarity score
     print("Image similarity:", score)
+    # defining the datatype for the differential score evaluated
     diff = (diff * 255).astype("uint8")
+    # removal of unnecessary noises through threshold application on the differential frame
     thresh = cv2.threshold(diff, 100, 255, cv2.THRESH_BINARY_INV)[1]
-
+    # find edges or contours from the thresholded frame
     contours, _ = cv2.findContours(
         thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = [c for c in contours if cv2.contourArea(c) > 50]
-
+    # condition for till contours are identified
     if len(contours):
         for c in contours:
             x, y, w, h = cv2.boundingRect(c)
@@ -35,15 +40,15 @@ def theftdetection(frame1, frame2):
     else:
         print("Nothing stolen")
         return 0
-
+    # view the different frames consisting of relevant frames
     cv2.imshow("Difference", thresh)
-    #cv2.imshow("Frame1", frame1)
+    # cv2.imshow("Frame1", frame1)
     cv2.imshow("Frame2", frame2)
-
+    # writing the frame 2 image to disk in the appropriate directory
     cv2.imwrite(
         "stolen/" + datetime.now().strftime("%y-%m-%d-%H-%M-%S") + ".jpg", frame2)
     event_trigger('Monitoring Functionality Session Ended!')
 
-
+    # exit logic
     cv2.waitKey(0)
     cv2.destroyAllWindows()
